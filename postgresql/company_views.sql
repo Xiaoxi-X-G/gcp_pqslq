e3472e843e519c49a4d0816fd5766321ac1be626
Xiaoxi Guo <xiaoxi.guo@anz.com>
2017-10-10 11:12:23 +1100

    create company views

---
+++ postgresql/company_views.sql
@@ -0,0 +1,20 @@
+﻿DROP view IF EXISTS credit_card_feature.cc_features_company_view;
+
+CREATE VIEW credit_card_feature.cc_features_company_view
+AS
+(
+	SELECT  cmpny.*
+	, round(cmpny.Txn_Cnt/cmpny.Cust_Cnt::DECIMAL, 2) txncnt_percust
+	, round(100*cmpny.custage_25_29_cnt/cmpny.cust_cnt::DECIMAL, 2) custage_25_29_pct	
+	FROM
+	(
+		SELECT *
+		FROM credit_card_feature.cc_features_company
+		-- where 
+	) cmpny
+);
+
+
+-- grand access permission 
+ALTER TABLE credit_card_feature.cc_features_company_view
+OWNER TO gcppoc;
